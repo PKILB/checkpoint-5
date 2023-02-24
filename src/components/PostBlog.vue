bd3fcd41ce87f455b84d39e1c9e77e520cd4d615
Payton Boone <paytonboone30@gmail.com>
2023-02-24 14:24:45 -0700

    Looking good now on the home page

--- src/components/PostBlog.vue
+++ src/components/PostBlog.vue
@@ -1,21 +1,18 @@
 <template>
-    <div class="col-12">
+    <div class="col-12 border border-dark rounded">
         <div class="row">
-            <div class="col-12">
-                <div class="row">
                     <!-- <router-link :to="{name: 'Profile', params: { profileId: post.creatorId}}"> -->
                     <div class="col-12 d-flex align-items-center my-3">
-                        <img :src="post.creator.picture" :alt="post.creator.name">
+                        <img class="profile-picture" :src="post.creator.picture" :alt="post.creator.name">
                         <h6 class="ms-3">{{ post.creator.name }}</h6>
                     </div>
-                    <!-- </router-link> -->
-                </div>
-
-                <div class="col-12">
                     <p>{{ post.body }}</p>
+                    <!-- </router-link> -->
+
+                <div class="col-12 text-end">
+                    <h2>Likes</h2>
                 </div>
             </div>
-        </div>
     </div>
 </template>
 
@@ -42,5 +39,10 @@ export default {
 
 
 <style lang="scss" scoped>
-
+.profile-picture {
+    height: 5vh;
+    width: 5vh;
+    border-radius: 50%;
+    box-shadow: 2px 2px 4px black;
+}
 </style>
